377554f5ed293703abec5c74360eb42984108ef9
hariom singh <140237983+Hariomsinghrajput@users.noreply.github.com>
2023-08-27 11:26:23 +0530

    commit

--- script.js
+++ script.js
@@ -2,22 +2,24 @@ function playGame(playerChoice) {
     const choices = ['rock', 'paper', 'scissors'];
     const computerChoice = choices[Math.floor(Math.random() * choices.length)];
   
-    const resultText = document.getElementById("resulttext");
-    if (playerChoice === computerChoice) {
-      resultText.textContent = "It's a tie!";
-    } else if (
-      (playerChoice === "rock" && computerChoice === "scissors") ||
-      (playerChoice === "paper" && computerChoice === "rock") ||
-      (playerChoice === "scissors" && computerChoice === "paper")
-    ) {
-      resultText.textContent = `You win! ${playerChoice} beats ${computerChoice}.`;
-    } else {
-      resultText.textContent = `You lose! ${computerChoice} beats ${playerChoice}.`;
+    const resultText = document.getElementById("result-text");
+    if (resultText) {
+      if (playerChoice === computerChoice) {
+        resultText.textContent = "It's a tie!";
+      } else if (
+        (playerChoice === "rock" && computerChoice === "scissors") ||
+        (playerChoice === "paper" && computerChoice === "rock") ||
+        (playerChoice === "scissors" && computerChoice === "paper")
+      ) {
+        resultText.textContent = `You win! ${playerChoice} beats ${computerChoice}.`;
+      } else {
+        resultText.textContent = `You lose! ${computerChoice} beats ${playerChoice}.`;
+      }
     }
   
-    // Redirect to result page
-    setTimeout(function() {
-      window.location.href = "result.html";
-    }, 1000);
+    const resultContainer = document.querySelector(".resultcontainer");
+    if (resultContainer) {
+      resultContainer.style.display = "block";
+    }
   }
   
